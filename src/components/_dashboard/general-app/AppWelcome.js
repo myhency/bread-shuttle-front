1a029aca3c053fb740557b44da119a1c300485bb
myhency <hency.yeo@gmail.com>
2021-10-25 22:54:00 +0900

    welcome box 수정

--- src/components/_dashboard/general-app/AppWelcome.js
+++ src/components/_dashboard/general-app/AppWelcome.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Link as RouterLink } from 'react-router-dom';
+// import { Link as RouterLink } from 'react-router-dom';
 // material
 import { styled } from '@mui/material/styles';
 import { Typography, Button, Card, CardContent } from '@mui/material';
@@ -36,17 +36,30 @@ export default function AppWelcome({ displayName }) {
           color: 'grey.800'
         }}
       >
+        {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
         <Typography gutterBottom variant="h4">
-          Welcome back,
+          환영합니다 😁
           <br /> {!displayName ? '...' : displayName}!
         </Typography>
 
         <Typography variant="body2" sx={{ pb: { xs: 3, xl: 5 }, maxWidth: 480, mx: 'auto' }}>
-          If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything
+          저희방의 컨셉은 ‘교육’입니다. 학습한걸 바탕으로 리딩 따라가는 방이니 아래 링크 필독하시고 참고하세요
         </Typography>
 
-        <Button variant="contained" to="#" component={RouterLink}>
-          Go Now
+        <Button
+          variant="contained"
+          // to="https://www.evernote.com/shard/s744/sh/9aa341ce-aa60-4df2-8775-05786fe5a74b/4ebc31f61275f61c72e6cecfd3aacc79"
+          // component={RouterLink}
+          // target="_blank"
+          // rel="noopener noreferrer"
+          onClick={() =>
+            window.open(
+              'https://www.evernote.com/shard/s744/sh/9aa341ce-aa60-4df2-8775-05786fe5a74b/4ebc31f61275f61c72e6cecfd3aacc79',
+              '_blank'
+            )
+          }
+        >
+          처음이신가요?
         </Button>
       </CardContent>
 
