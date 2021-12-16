b4f351b5eaafe41e4fc46b755a681b33c92e2a18
myhency <hency.yeo@gmail.com>
2021-12-16 09:18:59 +0900

    12월 텔레그램 주소 변경

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -124,7 +124,7 @@ export const PATH_STOCK_CALENDAR = 'http://www.paxnet.co.kr/stock/infoStock/issu
 export const PATH_NAVER_ECONOMIC_NEWS = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=101';
 export const PATH_COMPANY_GUIDE =
   'http://comp.fnguide.com/SVO2/asp/SVD_comp_calendar.asp?pGB=1&gicode=A005930&gcd=&gnm=&curdt=&ctp=&cID=&MenuYn=Y&ReportGB=&NewMenuID=701&stkGb=701';
-export const PATH_TELEGRAM_THIS_MONTH = 'https://t.me/joinchat/VG97nFWHF74zZWI1';
+export const PATH_TELEGRAM_THIS_MONTH = 'https://t.me/+hhkaTzslBxswZTll';
 
 export const PATH_ALPHA_LINK = 'https://alphasquare.co.kr/home/stock/stock-summary?code=';
 export const PATH_M_ALPHA_LINK = 'https://m.alphasquare.co.kr/service/chart?code=';
