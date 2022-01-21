178a69cc54c13823f3ef585286cd31b29f065886
myhency <hency.yeo@gmail.com>
2022-01-21 15:33:53 +0900

    v0.0.5-rc26 realeased

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/v1/platform',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'http://localhost:8081',
       changeOrigin: true
     })
   );
